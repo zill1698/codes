80eeece3659454468feb3fc0052752b9a1f2d3d4
zill1698 <128153096+zill1698@users.noreply.github.com>
2023-08-07 15:47:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+var randomNumber1=Math.floor(Math.random()*6)+1;
+var randomDiceImage="dice"+ randomNumber1 + ".jpg";
+var randomImageSource="images/"+randomDiceImage;
+var image1=document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomImageSource)
